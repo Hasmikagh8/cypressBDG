43c49381771d810fccfe3f470c15aa12a7c24ad9
Hasmikagh8 <hasmik.agh8@gmail.com>
2025-01-11 02:40:49 +0400

    FormAuthentication first commit

---
+++ cypress/Pages/FormAuthentication.js
@@ -0,0 +1,13 @@
+class AddRemoveElements {
+  NAMES = {
+    contentText: "Add/Remove Elements",
+    linkName:"/add_remove_elements/",
+    addElement:"Add Element",
+    deleteARE: "Delete",
+    loopCount:20,
+  };
+
+  
+}
+
+export const addRemoveElements = new AddRemoveElements();
