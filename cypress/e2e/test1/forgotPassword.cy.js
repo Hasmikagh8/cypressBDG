472dde2041ffc624d9e4bd130f914262c83a839a
Hasmikagh8 <hasmik.agh8@gmail.com>
2025-01-15 15:52:32 +0400

    adding css check in forgotPassword

--- cypress/e2e/test1/forgotPassword.cy.js
+++ cypress/e2e/test1/forgotPassword.cy.js
@@ -1,23 +1,36 @@
 import { basePage } from "../../Pages/Base";
 import { emailGenerator } from "../../utils/functions";
 import { forgotPassword } from "../../Pages/forgotPassword";
+import { COLORS } from "../../utils/colors";
 
 let textName = "";
 const emailName = emailGenerator(10);
 describe("dropdownList", () => {
-  it("Verify link and default value", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.contains(forgotPassword.NAMES.contentText).click();
+  });
+
+  it("Verify link, button's properties, clicking on button when e-mail is empty", () => {
     cy.url().should("include", `${forgotPassword.NAMES.forgotPasswordLink}`);
     cy.get(basePage.LOCATORS.content).should(
       "contain",
       forgotPassword.NAMES.contentText
     );
+
+    basePage
+      .getExample()
+      .get("#form_submit")
+      .should("have.css", "background-color", COLORS.colorsForgotPass.blueBack)
+      .get("i")
+      .should("have.contain", "Retrieve password")
+      .should("have.css", "font-style", "italic")
+      .and("have.css", "color", COLORS.colorsForgotPass.white).click();;
+    
+    cy.get("h1").should("contain", "Internal Server Error");
   });
 
   it("Verify Inserting an unique Email, checking the value and clicking on button", () => {
-    cy.visit("/");
-    cy.contains(forgotPassword.NAMES.contentText).click();
     basePage.getExample().get("#email").type(emailName);
     basePage
       .getExample()
@@ -31,6 +44,7 @@ describe("dropdownList", () => {
       expect(emailName).to.eq(textName);
     });
 
+    // clicking on button when e-mail is filled in
     basePage
       .getExample()
       .get("#form_submit")
