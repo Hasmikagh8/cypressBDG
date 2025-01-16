c73115dfb5fcae65165f28ddf30afba3ab632a8a
Hasmikagh8 <hasmik.agh8@gmail.com>
2025-01-16 11:52:04 +0400

    Form Authentication version1

--- cypress/Pages/FormAuthentication.js
+++ cypress/Pages/FormAuthentication.js
@@ -1,11 +1,24 @@
 class FormAuthentication {
   NAMES = {
     contentText: "Form Authentication",
-    formAuthLinkName:"login",
-    buttonLogin:"Login",
+    formAuthLinkName: "login",
+    buttonLogin: "Login",
+    title: "Login Page",
+    description: "This is where you can log into the secure area",
+    username: "Username",
+    password: "Password",
+    userNameInput: "Test1$",
+    passwordInput: "Password1$",
+    linkClose: ".close",
   };
 
-  
+  getLoginForm() {
+    return cy.get("form").get(".row").get("#login");
+  }
+
+  getFlash() {
+    return cy.get("#flash-messages").get("#flash");
+  }
 }
 
 export const formAuthentication = new FormAuthentication();
--- cypress/utils/colors.js
+++ cypress/utils/colors.js
@@ -3,5 +3,9 @@ export const COLORS = {
     colorsDisappear:{red:'rgb(218, 75, 75)',
     black: 'rgb(0, 0, 0)',
     black34: 'rgb(34, 34, 34)',
-    }, 
+    },
+    
+    blue1: 'rgb(43, 166, 203)',
+    white: 'rgb(255, 255, 255)',
+    red1: 'rgb(198, 15, 19)',
   }
