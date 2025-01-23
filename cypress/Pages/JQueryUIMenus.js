94e28f65e1496a278d416eee83d09710ac18db35
Hasmikagh8 <hasmik.agh8@gmail.com>
2025-01-23 13:36:59 +0400

    JqueryUIMenus adding CSV

--- cypress/Pages/JQueryUIMenus.js
+++ cypress/Pages/JQueryUIMenus.js
@@ -1,10 +1,25 @@
 class JQueryUIMenus {
-    NAMES = {
-      contentText: "JQuery UI Menus",
-      JQueryUIMenusLinkName:"jqueryui/menu" ,
-        
-    };  
-  }
-  
-  export const jQueryUIMenus = new JQueryUIMenus();
-  
+  NAMES = {
+    contentText: "JQuery UI Menus",
+    menuTitle: "JQueryUI - Menu",
+    jqueryTitle: "JQuery UI",
+    menuText:
+      "JQuery UI Menus are a nice UI element from a user perspective, but poses an interesting automation challenge since it requires mouse operations and synchronization between them.",
+    jQueryText:
+      "is many things, but one thing specifically that causes automation challenges is their set of Widgets",
+    menuLinkName: "menu",
+    jQuerylinkName: "jqueryui",
+    csvPath: "/downloads/menu.csv",
+  };
+
+  LOCATORS={enabled: "Enabled",
+    disabled: "Disabled",
+    downlaods: "Downloads",
+    backToJQueryUI: "Back to JQuery UI",
+    files: { pdf: "PDF", csv: "CSV", excel: "Excel" },
+    disabledID: "#ui-id-1",
+    description:".description",
+  };
+}
+
+export const jQueryUIMenus = new JQueryUIMenus();
--- cypress/e2e/test1/JQueryUIMenus.cy.js
+++ cypress/e2e/test1/JQueryUIMenus.cy.js
@@ -1,6 +1,6 @@
 import { jQueryUIMenus } from "../../Pages/JQueryUIMenus";
 import { basePage } from "../../Pages/Base";
-
+import Papa from "papaparse";
 
 const filePath = "cypress/fixtures/downloads/";
 describe("jQueryUIMenus", () => {
@@ -9,32 +9,76 @@ describe("jQueryUIMenus", () => {
     cy.contains(jQueryUIMenus.NAMES.contentText).click();
   });
 
-  it("Verify link and default value", () => {
-    cy.url().should("include", `${jQueryUIMenus.NAMES.JQueryUIMenusLinkName}`);
+  it("Verify Menu link, page and Excel file", () => {
+    cy.url().should(
+      "include",
+      `${jQueryUIMenus.NAMES.jQuerylinkName}/${jQueryUIMenus.NAMES.menuLinkName}`
+    );
     cy.get(basePage.LOCATORS.content).should(
       "contain",
       jQueryUIMenus.NAMES.contentText
     );
-
-    cy.get("#ui-id-3")
-      .find("a")
-      .should("have.attr", "href", "#")
-      .and("contain", "Enable");
-    cy.contains("Enable").click();
-    cy.contains("Downloads").click();
-    cy.contains("Excel").click();
-    cy.task("readdir", filePath).then((files) => {      
-      cy.readFile(`cypress/fixtures/downloads/${files[0]}`);
-      cy.parseXlsx(`cypress/fixtures/downloads/${files[0]}`).then(
-        (jsonData) => {
-          cy.log(JSON.stringify(jsonData[0]).data);
-          expect(JSON.stringify(jsonData[0].data)).to.contain("4");
-        }
-      );
-    });   
-                                                                
+    cy.get(jQueryUIMenus.LOCATORS.description)
+      .find("p")
+      .contains(jQueryUIMenus.NAMES.menuText);
+    cy.get("h3").should("contain", jQueryUIMenus.NAMES.menuTitle);
+    cy.contains(jQueryUIMenus.LOCATORS.enabled).click();
+    cy.contains(jQueryUIMenus.LOCATORS.downlaods).click();
+    cy.contains(jQueryUIMenus.LOCATORS.files.excel).click();
+    
+    cy.task("readdir", filePath).then((files) => {
+      cy.readFile(`${filePath}/${files[0]}`);
+      cy.parseXlsx(`${filePath}/${files[0]}`).then((jsonData) => {
+        cy.log(JSON.stringify(jsonData[0]).data);
+        expect(JSON.stringify(jsonData[0].data)).to.contain("4");
+      });
+    });
   });
-  afterEach(()=>
-    cy.task("deleteFolder", filePath)  
-  )
+
+  it("Verify csv file", () => {
+    cy.contains(jQueryUIMenus.LOCATORS.enabled).click();
+    cy.contains(jQueryUIMenus.LOCATORS.downlaods).click();
+    cy.contains(jQueryUIMenus.LOCATORS.files.csv).click();
+
+    cy.fixture(`${jQueryUIMenus.NAMES.csvPath}`).then((csvData) => {
+      // Use papaparse to parse the CSV data
+      Papa.parse(csvData, {
+        complete: (result) => {
+          // assertions
+          expect(result.data[0]["number of items"]).to.equal("4");
+          expect(result.data[0].subtotal).to.equal("4.00");
+          expect(result.data[0].tax).to.equal("0.13");
+          expect(result.data[0].total).to.equal("4.52");
+        },
+        header: true, // Treat the first row as the header
+      });
+    });
+  });
+
+  it("Verify that 'Disabled' is disabled", () => {
+    cy.contains(jQueryUIMenus.LOCATORS.disabled).should("be.visible");
+    cy.get(jQueryUIMenus.LOCATORS.disabledID).should(
+      "have.attr",
+      "aria-disabled",
+      "true"
+    );
+  });
+
+  it("Verify that 'Back to' link, Menu page and go to Menu", () => {
+    cy.contains(jQueryUIMenus.LOCATORS.enabled).click();
+    cy.contains(jQueryUIMenus.LOCATORS.backToJQueryUI).click();
+    cy.url().should("include", `${jQueryUIMenus.NAMES.jQuerylinkName}`);
+    cy.get("h3").should("contain", jQueryUIMenus.NAMES.jqueryTitle);
+    cy.get(jQueryUIMenus.LOCATORS.description)
+      .find("p")
+      .contains(jQueryUIMenus.NAMES.jQueryText);
+    //move to Menu page again
+    cy.get("li>a").click();
+    cy.url().should(
+      "include",
+      `${jQueryUIMenus.NAMES.jQuerylinkName}/${jQueryUIMenus.NAMES.menuLinkName}`
+    );
+  });
+
+  afterEach(() => cy.task("deleteFolder", filePath));
 });
