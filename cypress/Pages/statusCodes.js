341354b6c5c6476dd390d9fa6089343a3995776d
Hasmikagh8 <hasmik.agh8@gmail.com>
2025-01-23 17:21:43 +0400

    statusCodes first commit

---
+++ cypress/Pages/statusCodes.js
@@ -0,0 +1,16 @@
+class StatusCodes {
+  NAMES = {
+    contentText: "Status Codes",
+    statusCodesLinkName: "status_codes",
+    header: "Status Codes",
+    description:
+      "HTTP status codes are a standard set of numbers used to communicate from a web server to your browser to indicate the outcome of the request being made (e.g. Success, Redirection, Client Error, Server Error). For a complete list of status codes, go here. Some standard status codes you will run into include but are not limited to:",
+    statusCodes: [200, 301, 404, 500],
+    descSTCodes1: "This page returned a ",
+    descSTCodes2:
+      " status code. For a definition and common list of HTTP status codes, go here",
+    goBack: "here",
+  };
+}
+
+export const statusCodes = new StatusCodes();
--- cypress/e2e/test1/Main_Page.cy.js
+++ cypress/e2e/test1/Main_Page.cy.js
@@ -4,7 +4,7 @@ import { LOCATORS } from "../../utils/locators";
 describe("Main page testing", () => {
   it("Verify the functionality of Add/Remove button", () => {
     cy.visit("https://the-internet.herokuapp.com");
-    cy.get('ul').get('li')/*.should('have.attr', 'href')*/
+    cy.get('ul').get('li')
     .its('length').should('eq',44);
 
   });
